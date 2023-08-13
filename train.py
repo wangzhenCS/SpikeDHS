f23af17faf063fd94d1a8f2d6f1f5e8afd8756c5
wangzhenCS <130069028+wangzhenCS@users.noreply.github.com>
2023-08-13 09:09:05 +0800

    Update train.py
    增加带验证的数据集

--- train.py
+++ train.py
@@ -115,7 +115,11 @@ def main():
      # 把值转成Tensor
     transforms.ToTensor()])
 
-  dataset = ImageFolder("/kaggle/input/ddos-2019/Dataset-4/Dataset-4", 
+  #dataset = ImageFolder("/kaggle/input/ddos-2019/Dataset-4/Dataset-4", 
+  #                    transform=transform)
+  #dataset = ImageFolder("/kaggle/input/cse-cic-ids2018-for-snn", 
+  #                    transform=transform)
+  dataset = ImageFolder("/kaggle/input/nsl-kdd-for-snn/data", 
                       transform=transform)
 
   # 切分，训练集和验证集
